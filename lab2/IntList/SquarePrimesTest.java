273635493074b5568a72f0d38cd468409b082673
Grigoriy Emiliyanov <egrigoriy@gmail.com>
2023-11-21 16:41:13 +0300

    Lab 2 done

--- lab2/IntList/SquarePrimesTest.java
+++ lab2/IntList/SquarePrimesTest.java
@@ -17,4 +17,11 @@ public class SquarePrimesTest {
         assertEquals("14 -> 15 -> 16 -> 289 -> 18", lst.toString());
         assertTrue(changed);
     }
+    @Test
+    public void testSquarePrimesSimple2() {
+        IntList lst = IntList.of( 17, 18, 23);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("289 -> 18 -> 529", lst.toString());
+        assertTrue(changed);
+    }
 }
