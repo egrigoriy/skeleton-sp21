5dcb93d093eec49d7b94987a172c2fdce92bef89
Grigoriy Emiliyanov <egrigoriy@gmail.com>
2024-08-01 17:54:53 +0300

    Lab4b Git - done

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -10,15 +10,14 @@ public class Collatz {
      * @param an integer n
      * @return the next number in Collatz sequence
      */
-    /** Buggy implementation of nextNumber! */
     public static int nextNumber(int n) {
-        if (n  == 128) {
-            return 1;
-        } else if (n == 5) {
-            return 3 * n + 1;
-        } else {
-            return n * 2;
+        if (n % 2 == 0) {
+            return n / 2;
         }
+        if (n % 2 != 0) {
+            return 3 * n + 1;
+        }
+        return 1;
     }
 
     public static void main(String[] args) {
