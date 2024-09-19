5db247a63512e36cc5e4e61c4446f084a0b63ba6
Grigoriy Emiliyanov <egrigoriy@gmail.com>
2024-09-19 17:55:03 +0300

    Add javadocs to Commit

--- proj2/gitlet/Commit.java
+++ proj2/gitlet/Commit.java
@@ -5,37 +5,45 @@ import java.text.SimpleDateFormat;
 import java.util.*;
 
 /**
- *  TOD: It's a good idea to give a description here of what else this Class
- *  does at a high level.
+ *  Represents a Commit
  *
  *  @author Grigoriy Emiliyanov
  */
 public class Commit implements StorageObject, Serializable {
-    /**
-     *
-     * List all instance variables of the Commit class here with a useful
-     * comment above them describing what that variable represents and how that
-     * variable is used. We've provided one example for `message`.
-     */
-
-    /** The message of this Commit. */
-    private String message;
+    private final String message;
     private String uid;
-    private Date timestamp;
+    private final Date timestamp;
     private String firstParent = null;
     private String secondParent = null;
     private TreeMap<String, String> filesTable = new TreeMap<>();
 
+    /**
+     * Creates an initial commit.
+     * The timestamp for this initial commit will be 00:00:00 UTC, Thursday, 1 January 1970
+     */
     public Commit() {
         this.message = "initial commit";
         this.timestamp = new Date(0);
         setUid();
     }
 
+    /**
+     * Creates a commit with given message based on files in the given index.
+     * First parent is the current active commit and the second parent is null
+     * @param message
+     * @param index
+     */
     public Commit(String message, Index index) {
         this(message, index, null);
     }
 
+    /**
+     * Creates a commit with given message based on files in the given index.
+     * The created commit has active commit as first parent and the given second commit.
+     * @param message
+     * @param index
+     * @param secondParent
+     */
     public Commit(String message, Index index, String secondParent) {
         Branch currentBranch = new Branch();
         this.firstParent = currentBranch.getHeadCommitId();
@@ -46,6 +54,10 @@ public class Commit implements StorageObject, Serializable {
         setUid();
     }
 
+    /**
+     * Returns the string representation of this commit
+     * @return string
+     */
     @Override
     public String toString() {
         String result = "===" + "\n";
@@ -59,6 +71,10 @@ public class Commit implements StorageObject, Serializable {
         return  result;
     }
 
+    /**
+     * Returns the time stamp in formatted as Pacific Standard Time
+     * @return
+     */
     private String formatTimestamp() {
         String pattern = "EEE MMM d HH:mm:ss yyyy Z";
         SimpleDateFormat formatter = new SimpleDateFormat(pattern, Locale.ENGLISH);
@@ -66,34 +82,55 @@ public class Commit implements StorageObject, Serializable {
         return formatter.format(timestamp);
     }
 
+    /**
+     * Returns the uid of this commit
+     * @return uid
+     */
     public String getUid() {
         return uid;
     }
 
+    /**
+     * Returns the uid of the first parent of this commit
+     * @return uid
+     */
     public String getFirstParent() {
         return firstParent;
     }
 
-    public boolean hasFile(String fileName) {
-        return filesTable.containsKey(fileName);
-    }
-
+    /**
+     * Returns the hash corresponding to a file with given name from this commit
+     * @param fileName
+     * @return hash
+     */
     public String getFileHash(String fileName) {
         return filesTable.get(fileName);
     }
 
+    /**
+     * Returns the message associated with this commit
+     * @return message
+     */
     public String getMessage() {
         return message;
     }
 
-    private Date getTimestamp() {
-        return timestamp;
+    /**
+     * Return true if a file with given name is contained in this commit
+     * @param fileName
+     * @return boolean
+     */
+    public boolean hasFile(String fileName) {
+        return filesTable.containsKey(fileName);
     }
 
+    /**
+     * Sets the uid for this commit using its time stamp and message
+     */
     private void setUid() {
         this.uid = Utils.sha1(
-                this.getTimestamp().toString().getBytes(),
-                this.getMessage().getBytes());
+                this.timestamp.toString().getBytes(),
+                this.message.getBytes());
     }
 
     @Override
@@ -113,18 +150,35 @@ public class Commit implements StorageObject, Serializable {
         return Objects.hash(getUid());
     }
 
+    /**
+     * Returns the hash of the second parent of this commit
+     * @return uid
+     */
     public String getSecondParent() {
         return secondParent;
     }
 
+    /**
+     * Returns the files and corresponding hashes contained in this commit
+     * @return files and their hashes
+     */
     public TreeMap<String, String> getFilesTable() {
         return filesTable;
     }
 
+    /**
+     * Returns the names of the files contained in this commit
+     * @return a set of file names
+     */
     public Set<String> getFileNames() {
         return filesTable.keySet();
     }
 
+    /**
+     * Returns the hash of a file with given name contained in this commit
+     * @param fileName
+     * @return hash
+     */
     public String getHash(String fileName) {
         return filesTable.get(fileName);
     }
