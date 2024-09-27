23a20d2cb235ff852a77944cc299e9a39c505335
Grigoriy Emiliyanov <egrigoriy@gmail.com>
2024-09-27 17:19:55 +0300

    Lab7 just empty BSTMap.java

---
+++ lab7/bstmap/BSTMap.java
@@ -0,0 +1,67 @@
+package bstmap;
+
+import java.util.Iterator;
+import java.util.Set;
+import java.util.Spliterator;
+import java.util.function.Consumer;
+
+public class BSTMap<K, V> implements Map61B<K, V> {
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public boolean containsKey(K key) {
+        return false;
+    }
+
+    @Override
+    public V get(K key) {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public void put(K key, V value) {
+
+    }
+
+    @Override
+    public Set<K> keySet() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public V remove(K key) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public V remove(K key, V value) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Iterator<K> iterator() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void forEach(Consumer<? super K> action) {
+        Map61B.super.forEach(action);
+    }
+
+    @Override
+    public Spliterator<K> spliterator() {
+        return Map61B.super.spliterator();
+    }
+
+    public void printInOrder() {
+
+    }
+}
