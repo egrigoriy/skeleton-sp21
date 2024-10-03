75f4b2826222f43c1ac01e4031916a684f8a9635
Grigoriy Emiliyanov <egrigoriy@gmail.com>
2024-10-03 16:14:00 +0300

    Lab 8: refactoring + javadocs

--- lab8/hashmap/MyHashMap.java
+++ lab8/hashmap/MyHashMap.java
@@ -7,11 +7,9 @@ import java.util.*;
  *  access to elements via get(), remove(), and put() in the best case.
  *
  *  Assumes null keys will never be inserted, and does not resize down upon remove().
- *  @author YOUR NAME HERE
+ *  @author Grigoriy Emiliyanov
  */
 public class MyHashMap<K, V> implements Map61B<K, V> {
-
-
     /**
      * Protected helper class to store key/value pairs
      * The protected qualifier allows subclass access
@@ -24,40 +22,25 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
             key = k;
             value = v;
         }
-
-        @Override
-        public int hashCode() {
-            return key.hashCode() + value.hashCode();
-        }
-
-        @Override
-        public boolean equals(Object obj) {
-//            if (obj instanceof Node other) {
-//                return ((this.key == other.key) && (this.value == this.value));
-//            }
-            return false;
-        }
     }
+
+    private static final int DEFAULT_INITIAL_SIZE = 16;
+    private static final double DEFAULT_MAX_LOAD = 0.75;
+    private static final int RESIZE_FACTOR = 2;
 
     /* Instance Variables */
     private Collection<Node>[] buckets;
-    // You should probably define some more!
-    private int initialSize;
-    private double maxLoad;
     private HashSet<K> keys = new HashSet<>();
+    private int initialSize;
+    private double loadFactor;
 
     /** Constructors */
     public MyHashMap() {
-        this.initialSize = 16;
-        this.maxLoad = 0.75;
-        buckets = createTable(this.initialSize);
+        this(DEFAULT_INITIAL_SIZE, DEFAULT_MAX_LOAD);
     }
 
     public MyHashMap(int initialSize) {
-        this.initialSize = initialSize;
-        this.maxLoad = 0.75;
-        buckets = createTable(this.initialSize);
-
+        this(initialSize, DEFAULT_MAX_LOAD);
     }
 
     /**
@@ -65,11 +48,11 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
      * The load factor (# items / # buckets) should always be <= loadFactor
      *
      * @param initialSize initial size of backing array
-     * @param maxLoad maximum load factor
+     * @param loadFactor maximum load factor
      */
-    public MyHashMap(int initialSize, double maxLoad) {
+    public MyHashMap(int initialSize, double loadFactor) {
         this.initialSize = initialSize;
-        this.maxLoad = maxLoad;
+        this.loadFactor = loadFactor;
         buckets = createTable(this.initialSize);
     }
 
@@ -99,8 +82,7 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
      * OWN BUCKET DATA STRUCTURES WITH THE NEW OPERATOR!
      */
     protected Collection<Node> createBucket() {
-        MyHashMap<K, V> bb = new MyHashMapLLBuckets<>(initialSize, maxLoad);
-        return bb.createBucket();
+        return new LinkedList<>();
     }
 
     /**
@@ -113,16 +95,16 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
      * @param tableSize the size of the table to create
      */
     private Collection<Node>[] createTable(int tableSize) {
-        Collection<Node>[] result = new Collection[tableSize];
+        Collection<Node>[] table = new Collection[tableSize];
         for (int i = 0; i < tableSize; i++) {
-            result[i] = createBucket();
+            table[i] = createBucket();
         }
-        return result;
+        return table;
     }
 
     @Override
     public void clear() {
-        buckets = createTable(keys.size());
+        buckets = createTable(DEFAULT_INITIAL_SIZE);
         keys.clear();
     }
 
@@ -133,14 +115,31 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
 
     @Override
     public V get(K key) {
-        int index = Math.floorMod(asciiToInt((String)key), buckets.length);
-        Collection<Node> bucket = buckets[index];
-        for (Node node : bucket) {
-            if (key.equals(node.key)) {
-                return node.value;
-            }
+        Node node = findNode(key);
+        if (node == null) {
+            return null;
         }
-        return null;
+        return node.value;
+    }
+
+    /**
+     * Returns the hash corresponding to given key with modulo to the buckets length.
+     * AsciiToInt conversion is used.
+     * @param key
+     * @return hash code
+     */
+    private int getIndexFromKey(K key) {
+        return Math.floorMod(asciiToInt((String)key), buckets.length);
+    }
+
+    /**
+     * Returns the bucket corresponding to a given key
+     * @param key
+     * @return bucket
+     */
+    private Collection<Node> getBucket(K key) {
+        int index = getIndexFromKey(key);
+        return buckets[index];
     }
 
     @Override
@@ -150,24 +149,50 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
 
     @Override
     public void put(K key, V value) {
-        keys.add(key);
-        int index = Math.floorMod(asciiToInt((String)key), buckets.length);
-        if (index > buckets.length) {
-            resize(index);
+        if (isOverloaded()) {
+            resize(buckets.length * RESIZE_FACTOR);
         }
-        Collection<Node> bucket = buckets[index];
-        for (Node node : bucket) {
-            if (key.equals(node.key)) {
-                node.value = value;
-                return;
-            }
+        Node node = findNode(key);
+        if (node != null) {
+            node.value = value;
+            return;
         }
         Node nodeToPut = createNode(key, value);
+        Collection<Node> bucket = getBucket(key);
         bucket.add(nodeToPut);
+        keys.add(key);
     }
 
+    /**
+     * Returns true if this hash map is overloaded,
+     * i.e. size exceeds the current number of buckets * load factor
+     * @return boolean
+     */
+    private boolean isOverloaded() {
+        return size() > loadFactor * buckets.length;
+    }
+
+    /**
+     * Returns the node from this hash map having given key.
+     * If the key is not present, then returns null.
+     * @param key
+     * @return a node if found, otherwise null
+     */
+    private Node findNode(K key) {
+        for (Node node : getBucket(key)) {
+            if (key.equals(node.key)) {
+                return node;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Resizes this hash map buckets table
+     * @param size
+     */
     private void resize(int size) {
-        Collection<Node>[] newBuckets = createTable(size * 2);
+        Collection<Node>[] newBuckets = createTable(size);
         System.arraycopy(buckets, 0, newBuckets, 0, buckets.length);
         buckets = newBuckets;
     }
@@ -179,30 +204,41 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
 
     @Override
     public V remove(K key) {
-        keys.remove(key);
-        int index = Math.floorMod(asciiToInt((String)key), buckets.length);
-        Collection<Node> bucket = buckets[index];
-        for (Node node : bucket) {
-            if (key.equals(node.key)) {
-                V value = node.value;
-                bucket.remove(node);
-                return value;
-            }
+        Node node = findNode(key);
+        if (node == null) {
+            return null;
         }
-        return null;
+        keys.remove(key);
+        V value = node.value;
+        Collection<Node> bucket = getBucket(key);
+        bucket.remove(node);
+        return value;
     }
 
     @Override
     public V remove(K key, V value) {
-        throw new  UnsupportedOperationException();
+        Node node = findNode(key);
+        if (node == null) {
+            return null;
+        }
+        if (!value.equals(node.value)) {
+            return null;
+        }
+        return remove(key);
     }
 
     @Override
     public Iterator<K> iterator() {
-        return null;
+        return keys.iterator();
     }
 
-    public static int asciiToInt(String s) {
+    /**
+     * Returns the integer corresponding to given string
+     * Taken from Lecture slides: "cs61b 2020 ds5 lec19 hashing"
+     * @param s
+     * @return hash
+     */
+    private static int asciiToInt(String s) {
         int intRep = 0;
         for (int i = 0; i < s.length(); i += 1) {
             intRep = intRep * 126;
@@ -210,5 +246,4 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
         }
         return intRep;
     }
-
 }
